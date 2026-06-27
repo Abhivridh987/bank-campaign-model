cf497d320842c9a776e4f1ee95de2ce114d541d1
Abhivridh <abhivridh2@gmail.com>
2026-06-27 18:11:30 +0530

    Add CORS middleware to FastAPI application

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 import pandas as pd
@@ -7,6 +8,13 @@ import uvicorn
 import os
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 model = joblib.load("random_forest_classifier.pkl")
 scaler = joblib.load("standard_scaler.pkl")
